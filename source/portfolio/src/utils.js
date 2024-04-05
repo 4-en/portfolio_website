f6863d18b49bdba887bd5fadf94ae4824d6b09a1
4-en <ben.schumacher0@gmail.com>
2024-04-05 13:07:50 +0200

    add toggleTheme functions to util for svelte components

---
+++ source/portfolio/src/utils.js
@@ -0,0 +1,52 @@
+// copied from toggleTheme.js
+
+// get the current theme
+function getTheme() {
+    // check if class theme is set, light or dark
+    let classList = document.body.classList;
+    let classTheme = classList.contains('light') ? 'light' : classList.contains('dark') ? 'dark' : null;
+    if (classTheme) {
+        return classTheme;
+    }
+
+    // check if local storage has a theme set
+    let localTheme = localStorage.getItem('theme');
+    if (localTheme) {
+        return localTheme;
+    }
+
+    // check if the browser has a theme set
+    let browserTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : null;
+    browserTheme = browserTheme || window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : null;
+    if (browserTheme) {
+        return browserTheme;
+    }
+
+    // default to dark theme
+    return 'dark';
+}
+
+// set the theme
+function setTheme(theme) {
+    // set the class theme
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+
+    // print classList for debugging
+    console.log(document.body.classList);
+
+    // set the local storage theme
+    localStorage.setItem('theme', theme);
+}
+
+// toggle the theme
+function toggleTheme() {
+    let theme = getTheme();
+    console.log(`Current theme is ${theme}`);
+    theme = theme === 'light' ? 'dark' : 'light';
+    setTheme(theme);
+
+    console.log(`Theme set to ${theme}`);
+}
+
+export { getTheme, setTheme, toggleTheme };
